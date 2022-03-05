fa1d5af9120bc146ff6913f2f16190862760a459
julysein723 <julysein723@gmail.com>
2022-03-05 22:52:45 +0900

    chore : heroku DB 설정

--- unithon_backend/settings.py
+++ unithon_backend/settings.py
@@ -193,6 +193,6 @@ CORS_ALLOW_CREDENTIALS = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
