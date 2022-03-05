2aea4c3202af436217340f2a8c5f0a113a94515e
youjeong2 <progress.uj11@gmail.com>
2022-03-05 19:23:51 +0900

    chore : url삭제

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView
 )
 
-from accounts.views import CustomUserCreate, CheckDuplicatedId, ReturnId
+from accounts.views import CustomUserCreate, CheckDuplicatedId
 
 app_name = 'users'
 
@@ -13,6 +13,4 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('checkid/', CheckDuplicatedId.as_view(), name='check_duplicated_id'),
-    path('returnid/', ReturnId.as_view(), name='return_id')
-
 ]
