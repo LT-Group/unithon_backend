e894b08f9c1c52645b970b044440c47fd6f57b5c
julysein723 <julysein723@gmail.com>
2022-03-05 15:53:29 +0900

    chore : TIME ZONE 한국 시간 설정 build : CORS ORIGIN 설정 build : restframework 설정 build : simple-jwt token 설정 feat : 로그인(토큰 발급), token refresh API feat : 회원가입 API feat : 비밀번호 암호화

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
+from accounts.views import CustomUserCreate
 
 app_name = 'users'
 
 urlpatterns = [
-
+    path('register/', CustomUserCreate.as_view(), name="create_user"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
