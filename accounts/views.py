9170e98ad72d76ff13c7a0fc393bfd9e9a54421b
youjeong2 <progress.uj11@gmail.com>
2022-03-05 19:10:52 +0900

    feat : 아이디 중복확인

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from accounts.models import User
 from accounts.serializers import RegisterUserSerializer
 
 class CustomUserCreate(APIView):
@@ -15,3 +16,19 @@ class CustomUserCreate(APIView):
             if newuser:
                 return Response(status=status.HTTP_201_CREATED)
         return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CheckDuplicatedId(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        id = request.data['id']
+        try:
+            user = User.objects.get(user_id=id)
+            if (user):
+                return Response(dict(result=True))
+            else:
+                return Response(dict(result=False))
+        except:
+            return Response(dict(result=False))
+
