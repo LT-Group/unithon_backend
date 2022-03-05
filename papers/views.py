7408cf78c32ed4e15a94d14b2dc3e5442e06194c
julysein723 <julysein723@gmail.com>
2022-03-05 21:16:26 +0900

    fix : file full url로 return 될 수 있게 수정

--- papers/views.py
+++ papers/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django.conf import settings
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -32,7 +33,7 @@ class GetPaper(APIView):
             n = random.randint(0, paper_left-1)
             paper = paper_queryset[n]
             if paper.test_audio_file:
-                file_url = paper.test_audio_file.url
+                file_url = str(settings.MEDIA_URL + paper.test_audio_file.url)
             else :
                 file_url = None
             #시험지 id 와
