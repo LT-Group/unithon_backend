3f25f5675afb63b2dd75fc517b0ebdbcb8c970d9
julysein723 <julysein723@gmail.com>
2022-03-05 18:24:49 +0900

    feat : 6. 시험지 불러오기 feat : 7. 시험 결과 포스팅하기 feat : 9. 시험지 결과 상세페이지

--- papers/urls.py
+++ papers/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
+from papers.views import GetPaper, SavingPaper, PostPaper, GetPaperDetail
+
 app_name = 'papers'
 
 urlpatterns = [
-
+    path('get_paper/<int:user_id>/', GetPaper.as_view(), name="get_paper"),
+    path('post_paper/', PostPaper.as_view(), name="post_paper"),
+    path('get_paper_detail/<int:user_id>/<int:paper_id>/', GetPaperDetail.as_view()),
+    path('saving_paper/', SavingPaper.as_view(), name="saving_paper"),
 ]
