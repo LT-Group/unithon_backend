1df6faf74b998d4794420ed381c2be432a8128b3
youjeong2 <progress.uj11@gmail.com>
2022-03-05 14:21:23 +0900

    feat : 개발 환경세팅 및 모델 작성

---
+++ papers/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+from unithon_backend import settings
+
+
+class Paper(models.Model):
+    test_audio_file = models.FileField(upload_to='test_audio_file/%Y/%m/%d', help_text="테스트 음성파일")
+
+
+class PaperUser(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='paper_user')
+    paper = models.ForeignKey('papers.Paper', help_text='시험', on_delete=models.CASCADE, related_name='paper_user')
+    total_score = models.IntegerField(help_text='총점', default=0)
+
+
+class Question(models.Model):
+    paper = models.ForeignKey('papers.Paper', help_text='시험', on_delete=models.CASCADE, related_name='question')
+    answer = models.CharField(help_text='정답', max_length=50)
+    solved_count = models.IntegerField(help_text='푼 사람 수', default=0)
+    wrong_count = models.IntegerField(help_text='틀린 사람 수', default=0)
+    wrong_answer_rate = models.FloatField(help_text='오답률', default=0)
+
+
+class QuestionUser(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, help_text='유저가 푼 시험 문제', on_delete=models.CASCADE,
+                             related_name='question_user')
+    question = models.ForeignKey('papers.Question', help_text='시험 문제', on_delete=models.CASCADE,
+                                 related_name='question_user')
+    answer_user = models.CharField(help_text='유저가 작성한 정답', max_length=50)
+    is_correct = models.BooleanField(help_text='정답 여부')
+
--- unithon_backend/settings.py
+++ unithon_backend/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #project_apps
-    # 'accounts',
+    'accounts',
+    'papers',
+
 ]
 
 MIDDLEWARE = [
@@ -123,6 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+AUTH_USER_MODEL = 'accounts.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
